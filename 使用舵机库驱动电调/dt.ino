907869588f3cb3e4f0e523c7230fc08072933f6f
玄学 <92659074+KW-T@users.noreply.github.com>
2023-08-11 01:05:59 +0800

    Add files via upload

---
+++ 使用舵机库驱动电调/dt.ino
@@ -0,0 +1,32 @@
+#include <Servo.h>
+
+Servo myServo;
+int c1 = 0;  // declares variable c1
+int c2 = 0;  // declares variable c1
+String comStr = "";
+int comInt = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);          // start serial port
+  while (Serial.read() >= 0) {}  // clear serial port's buffer
+  //pinMode(pot, INPUT);
+  myServo.attach(13);               // attach myServo to GPIO_09
+  myServo.writeMicroseconds(1000);  // set initial PWM to minimal
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // if (Serial.available() > 0) {
+  // listen the Serial port, run the code when something catched..
+  delay(10);
+  comStr = Serial.readString();            // read out the string
+  comInt = comStr.toInt();                 // convert the string to integer
+  comInt = constrain(comInt, 1000, 2000);  // limit the integer between to 1000 and 2000
+  c1 = analogRead(A6);
+  c2 = (c1 + 977);
+  Serial.println(c2);             // show the integer number on Serial Monitor
+  myServo.writeMicroseconds(c2);  // write the integer number to Servo in unit of micro-second
+                                  // }
+}
+
